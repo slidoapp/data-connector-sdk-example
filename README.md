5266b90a7e3cca5a1de9d93908f7e766539db8db
mr.Shu <mr@shu.io>
2020-11-27 16:25:03 +0100

    add: Sample query.py script
    * Add a sample script that queries Slido-managed Athena in an
      account-specific WorkGroup.
    
    * Update the README with instructions how to install install the
      requirements and run the example.
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# data-connector-sdk-example
-Slido Data Connector SKD Example
+# Slido Data Connector SKD Example
+
+This repository contains a simple example of running queries via the
+[Slido Data Connector](https://docs.google.com/document/d/19PLz-NZPXOm5YDNXML9P419BRek7QQymExI0gxlxK9o/edit)
+(currently in preview) using [AWS SDK](https://aws.amazon.com/sdk-for-python/) in Python.
+
+### Requirements
+
+is `boto3` (tested with version 1.16.25).
+
+You can install it (along its dependencies) by running the following command:
+
+        $ pip install -r requirements.txt
+
+### Running the example
+
+In order to run the example, the following environment variables need to be
+set:
+
+        $ export AWS_ACCESS_KEY_ID=<the value you received>
+        $ export AWS_SECRET_ACCESS_KEY=<the value you received>
+        $ export SLIDO_ACCOUNT_UUID=<the value you received>
+
+Provided all the requirements are installed, the example can be executed by
+running
+
+        $ python query.py
+
---
+++ query.py
@@ -0,0 +1,71 @@
+import boto3
+import os
+import time
+import pprint
+
+AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+SLIDO_ACCOUNT_UUID = os.environ['SLIDO_ACCOUNT_UUID']
+
+
+def athena_query(athena,
+                 query,
+                 database,
+                 workgroup,
+                 s3_location,
+                 max_iters=20,
+                 sleep_time=3):
+
+    response = athena.start_query_execution(
+        QueryString=query,
+        QueryExecutionContext={
+            'Database': database
+        },
+        ResultConfiguration={
+            'OutputLocation': s3_location
+        },
+        WorkGroup=workgroup
+    )
+
+    execution_id = response['QueryExecutionId']
+
+    while max_iters > 0:
+        max_iters -= 1
+
+        response = athena.get_query_execution(QueryExecutionId=execution_id)
+        status = response['QueryExecution']['Status']['State']
+        if status == 'FAILED':
+            return False
+
+        if status == 'SUCCEEDED':
+            return athena.get_query_results(
+                QueryExecutionId=execution_id
+            )
+
+        time.sleep(sleep_time)
+
+    raise Exception("Reached the number of max_iters.")
+
+
+if __name__ == '__main__':
+
+    params = {
+        'region': 'eu-west-1',
+        'database': SLIDO_ACCOUNT_UUID,
+        'workgroup': SLIDO_ACCOUNT_UUID,
+        'bucket': 'slido-dwh-customer-query-results',
+        'query': 'SELECT * FROM events LIMIT 10'
+    }
+
+    session = boto3.Session()
+    athena = session.client('athena',
+                            region_name=params["region"],
+                            aws_access_key_id=AWS_ACCESS_KEY,
+                            aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    s3_location = f's3://{params["bucket"]}/{params["database"]}'
+
+    pprint.pprint(athena_query(athena,
+                               params['query'],
+                               params['database'],
+                               params['workgroup'],
+                               s3_location))
